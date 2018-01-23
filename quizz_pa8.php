499328c3cc6bdaa661fe085956ba31cf41cc44cd
Unknown <gallieni33@hotmail.fr>
2018-01-23 13:20:40 +0100

    Declare plugin Quizzy

---
+++ quizz_pa8.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Quizzy
+Plugin URI: http://13.58.216.136/
+Description : It's easy to build quizz with Quizzy
+Version : 0.1
+Author : WAYSY CORP
+Author URI: http://13.58.216.136/
+License: GPL2
+*/
