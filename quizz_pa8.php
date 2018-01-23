dd27c1f0c525c987db8703536bf714596fdddc47
Unknown <gallieni33@hotmail.fr>
2018-01-23 13:30:31 +0100

    espace bug

--- quizz_pa8.php
+++ quizz_pa8.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: Quizzy
 Plugin URI: http://13.58.216.136/
-Description : It's easy to build quizz with Quizzy
-Version : 0.1
-Author : WAYSY CORP
+Description: It's easy to build quizz with Quizzy
+Version: 0.1
+Author: WAYSY CORP
 Author URI: http://13.58.216.136/
 License: GPL2
 */
